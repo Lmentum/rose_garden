33cf3ab8de7bf13dad74879e1de1d3c3aac42914
Liam Lindhe-Johan <liamljohan@gmail.com>
2025-10-26 20:08:55 -0700

    tickrate fix again :/

--- Server.js
+++ Server.js
@@ -105,7 +105,7 @@ io.on("connection", socket => {
 setInterval(() => {
     updateGame();
     io.emit("state", { players, ball });
-}, 1000 / 70);
+}, 1000 / 60);
 
 function handleInput(player, input) {
     const speed = 3;
